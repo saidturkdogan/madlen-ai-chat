4d42b82b64b05a6c0cd2aa654ba855d9a8467be6
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2025-12-18 03:59:04 +0300

    fix: add Clerk authentication middleware

--- fe/middleware.ts
+++ fe/middleware.ts
@@ -13,4 +13,5 @@ export const config = {
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
         '/(api|trpc)(.*)',
     ],
+    runtime: "nodejs",
 };
