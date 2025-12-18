a0307ac8c3dd3d21c3b8177f833586c88f56d218
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2025-12-18 04:47:14 +0300

    feat: add Dockerfile and Next.js configuration for standalone deployment.

--- fe/next.config.mjs
+++ fe/next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    },
 };
 
 export default nextConfig;
