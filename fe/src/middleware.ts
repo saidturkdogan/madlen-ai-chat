2edba87abdf34b660a597203b894bc26a0bfadc0
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2025-12-18 04:07:29 +0300

    fix: Add Clerk authentication middleware to protect routes.

--- fe/src/middleware.ts
+++ fe/src/middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/']);
+const isPublicRoute = createRouteMatcher([
+    '/',
+    '/sign-in(.*)',
+    '/sign-up(.*)'
+]);
 
 export default clerkMiddleware(async (auth, request) => {
     if (!isPublicRoute(request)) {
@@ -10,8 +14,7 @@ export default clerkMiddleware(async (auth, request) => {
 
 export const config = {
     matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
         '/(api|trpc)(.*)',
     ],
-    runtime: "nodejs",
 };
